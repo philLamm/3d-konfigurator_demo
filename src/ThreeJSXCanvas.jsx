4cd9cbfac99f004b7c62a6a59ccf1f5894fe28f8
Philipp Lammerding <timii99@web.de>
2019-05-06 21:54:55 +0200

    buildable

--- src/ThreeJSXCanvas.jsx
+++ src/ThreeJSXCanvas.jsx
@@ -34,30 +34,30 @@ function Octahedron() {
   )
 }
 
-function Stars() {
-  let group = useRef()
-  let theta = 0
-  useRender(() => {
-    // Some things maybe shouldn't be declarative, we're in the render-loop here with full access to the instance
-    const r = 5 * Math.sin(THREE.Math.degToRad((theta += 0.1)))
-    const s = Math.cos(THREE.Math.degToRad(theta * 2))
-    group.current.rotation.set(r, r, r)
-    group.current.scale.set(s, s, s)
-  })
-  const [geo, mat, vertices, coords] = useMemo(() => {
-    const geo = new THREE.SphereBufferGeometry(1, 10, 10)
-    const mat = new THREE.MeshBasicMaterial({ color: new THREE.Color('lightblue') })
-    const coords = new Array(2000).fill().map(i => [Math.random() * 800 - 400, Math.random() * 800 - 400, Math.random() * 800 - 400])
-    return [geo, mat, vertices, coords]
-  }, [])
-  return (
-    <group ref={group}>
-      {coords.map(([p1, p2, p3], i) => (
-        <mesh key={i} geometry={geo} material={mat} position={[p1, p2, p3]} />
-      ))}
-    </group>
-  )
-}
+// function Stars() {
+//   let group = useRef()
+//   let theta = 0
+//   useRender(() => {
+//     // Some things maybe shouldn't be declarative, we're in the render-loop here with full access to the instance
+//     const r = 5 * Math.sin(THREE.Math.degToRad((theta += 0.1)))
+//     const s = Math.cos(THREE.Math.degToRad(theta * 2))
+//     group.current.rotation.set(r, r, r)
+//     group.current.scale.set(s, s, s)
+//   })
+//   const [geo, mat, vertices, coords] = useMemo(() => {
+//     const geo = new THREE.SphereBufferGeometry(1, 10, 10)
+//     const mat = new THREE.MeshBasicMaterial({ color: new THREE.Color('lightblue') })
+//     const coords = new Array(2000).fill().map(i => [Math.random() * 800 - 400, Math.random() * 800 - 400, Math.random() * 800 - 400])
+//     return [geo, mat, vertices, coords]
+//   }, [])
+//   return (
+//     <group ref={group}>
+//       {coords.map(([p1, p2, p3], i) => (
+//         <mesh key={i} geometry={geo} material={mat} position={[p1, p2, p3]} />
+//       ))}
+//     </group>
+//   )
+// }
 
 function ThreeJSXCanvas() {
   return (
@@ -65,7 +65,7 @@ function ThreeJSXCanvas() {
     <ambientLight color="lightblue" />
     <pointLight color="white" intensity={1} position={[10, 10, 10]} />
     <Octahedron />
-    <Stars />
+    {/* <Stars /> */}
   </Canvas>
   );
 }
