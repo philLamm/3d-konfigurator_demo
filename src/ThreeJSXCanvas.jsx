fc51ca2529a7b2803600b0b7d75133fb031a560d
jastech-digital <info@jastech-solutions.de>
2019-05-06 13:52:34 +0200

    update

--- src/ThreeJSXCanvas.jsx
+++ src/ThreeJSXCanvas.jsx
@@ -6,7 +6,7 @@ import { Canvas, useRender } from 'react-three-fiber'
 // A React animation lib, see: https://github.com/react-spring/react-spring
 
 import { useSpring, animated } from 'react-spring/three'
-import './styles.css'
+
 
 function Octahedron() {
   const [active, setActive] = useState(false)
@@ -34,12 +34,38 @@ function Octahedron() {
   )
 }
 
-function ThreeJSXCanvas({ vertices, color }) {
+function Stars() {
+  let group = useRef()
+  let theta = 0
+  useRender(() => {
+    // Some things maybe shouldn't be declarative, we're in the render-loop here with full access to the instance
+    const r = 5 * Math.sin(THREE.Math.degToRad((theta += 0.1)))
+    const s = Math.cos(THREE.Math.degToRad(theta * 2))
+    group.current.rotation.set(r, r, r)
+    group.current.scale.set(s, s, s)
+  })
+  const [geo, mat, vertices, coords] = useMemo(() => {
+    const geo = new THREE.SphereBufferGeometry(1, 10, 10)
+    const mat = new THREE.MeshBasicMaterial({ color: new THREE.Color('lightblue') })
+    const coords = new Array(2000).fill().map(i => [Math.random() * 800 - 400, Math.random() * 800 - 400, Math.random() * 800 - 400])
+    return [geo, mat, vertices, coords]
+  }, [])
+  return (
+    <group ref={group}>
+      {coords.map(([p1, p2, p3], i) => (
+        <mesh key={i} geometry={geo} material={mat} position={[p1, p2, p3]} />
+      ))}
+    </group>
+  )
+}
+
+function ThreeJSXCanvas() {
   return (
     <Canvas>
     <ambientLight color="lightblue" />
     <pointLight color="white" intensity={1} position={[10, 10, 10]} />
     <Octahedron />
+    <Stars />
   </Canvas>
   );
 }
